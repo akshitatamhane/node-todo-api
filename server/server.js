852eed918792a5b33cad43ff520c2188ee0b47a5
Akshita Tamhane <akshitatamhane93@gmail.com>
2018-09-11 11:55:14 +0530

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express =require('express');
 var bodyParser=require('body-parser');
+var {ObjectID}=require('mongodb');
 
 var app =express();
 
@@ -27,7 +28,24 @@ app.get('/todos',(req,res)=>{
     });
 });
 
+// GET /todos/1234324
+app.get('/todos/:id',(req,res)=>{
+    var id=req.params.id;
+    
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send();
+    }
 
+    Todo.findById(id).then((todo)=>{
+     if(!todo){
+         return res.status(404).send();
+     }
+
+     res.send({todo});
+    }).catch((e)=>{
+        res.status(400).send();
+    });
+});
 
 
 app.listen(3000,() =>{
